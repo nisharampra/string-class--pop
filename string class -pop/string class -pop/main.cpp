54e0b42ee38e4132e7dd30dc58cb3af72c1a939b
Nisha Ramprasath <115043583+nisharampra@users.noreply.github.com>
2024-01-08 23:06:57 +0800

    Add files via upload

---
+++ string class -pop/string class -pop/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  string class -pop
+//
+//  Created by Nisha Ramprasath on 3/11/23.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    string str="prigramming";
+    str.pop_back();
+    cout<<str<<endl;
+    return 0;
+}
